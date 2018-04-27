2f354887a19874bac8c1c97c6664a06d0d4c6717
Alexey Kalaverin <alexey@kalaverin.ru>
2018-04-28 03:55:24 +0500

    Add brew ggrep.

--- plugin.zsh
+++ plugin.zsh
@@ -1,23 +1,34 @@
-if [[ "$OSTYPE" == "freebsd"* ]]; then
-    grep_bin='/usr/local/bin/grep'
-    if [[ ! -f $grep_bin ]]; then
-        echo "Try install GNU grep: pkg install gnugrep"
-        return
+if [ -n "$(uname | grep -i freebsd)" ]; then
+    grep='/usr/local/bin/grep'
+    if [[ ! -f $grep ]]; then
+        echo "Install GNU grep: pkg install gnugrep"
+        return 1
     fi
     pattern='s/^([^:]+:[^:]+):\s*(.*)/\1:  \2/'
+
+elif [ -n "$(uname | grep -i darwin)" ]; then
+    grep='/usr/local/bin/ggrep'
+    if [[ ! -f $grep ]]; then
+        echo "Install GNU grep: brew install grep"
+        return 1
+    fi
+    pattern='s/^([^:]+:[^:]+):\s*(.*)/\x1b[35m\1\x1b[0m:  \2/'
+
 else
-    grep_bin='grep'
+    grep='grep'
     pattern='s/^([^:]+:[^:]+):\s*(.*)/\x1b[35m\1\x1b[0m:  \2/'
 fi
+
 
 function :fuzzy-search-and-edit:get-files() {
     local directory="$1"
     local fifo="$2"
     cd "$directory"
-    command $grep_bin -r -nEHI '[[:alnum:]]' "." --exclude-dir=".git" \
+    command $grep -r -nEHI '[[:alnum:]]' "." --exclude-dir=".git" \
         | cut -b3- \
         | command sed -ru $pattern > "$fifo"
 }
+
 
 function :fuzzy-search-and-edit:abort-job() {
     local match
@@ -27,6 +38,7 @@ function :fuzzy-search-and-edit:abort-job() {
 
     async_flush_jobs ":fuzzy-search-and-edit:worker"
 }
+
 
 function fuzzy-search-and-edit() {
     local dir="$(mktemp -d /tmp/fuzzy-search-and-edit.XXXXXX)"
@@ -51,7 +63,7 @@ function fuzzy-search-and-edit() {
     fi
 
     rm -r "$dir"
-    find /tmp -maxdepth 1 -name "fuzzy-search-and-edit.*" -user yaakov -type d -cmin +60 -exec rm -rf {} \;
+    find /tmp -maxdepth 1 -name "fuzzy-search-and-edit.*" -user yaakov -type d -mmin +30 -exec rm -rf {} \;
 
     zle -I
 }
